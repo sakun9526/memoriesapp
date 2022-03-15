0e9db381a50bca54ac76bbf6397cf3f0109198d1
sakun9526 <sakunrathnayaka@yahoo.com>
2022-03-15 05:49:02 +0530

    index.js initialized and database connection setup completed

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+//environment variables
+import 'dotenv/config';
+
+const app = express();
+
+// maximum request body size set to 30mb
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+
+const PORT = process.env.PORT||5000;
+
+mongoose.connect(process.env.CONNECTION_URL)
+.then(() => app.listen(PORT, () => console.log(`Server is running on ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+
+
